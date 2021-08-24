4e6b8d840750af7da98091ba159c69b1d74ba510
uzairlatif90 <33296015+uzairlatif90@users.noreply.github.com>
2021-08-24 17:36:46 +0200

    Delete ClusterScripts directory

--- ClusterScripts/exe_Int.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-root -q -b -l '/home/uzairlatif/Documents/ARA_Stuff/TestInterSPICEdata/ClusterScripts/ReconstructSPICEevents.C("'$1'"',''$2'',''$3')'
-#root -q -b -l '/data/user/ulatif/templatematch/GetBrianEvents.C("'$1'"',''$2'',''$3')'
